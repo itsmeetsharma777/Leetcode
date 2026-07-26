bbee32dba7e514e31ed8f142af4780ed48c8961d
Meet Sharma <itsmeetcodes@gmail.com>
2026-07-26 18:20:17 +0530

    Time: 5 ms (20.21%) | Memory: 8.5 MB (84.24%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,17 +2,13 @@ class Solution {
 public:
     int reverse(int x) {
         long long reverse=0;
-        int minvalue=-2147483648;
-        int maxvalue=2147483647;
-        int orignal=x;
         while(x>0||x<0){
             int remainder=x%10;
             reverse=(reverse*10)+remainder;
-            x=x/10;
-            if (reverse > maxvalue || reverse < minvalue){
+            if (reverse>INT_MAX  || reverse<INT_MIN ){
                 return 0;
             }
-
+            x=x/10;
         }
         return reverse;
     }
