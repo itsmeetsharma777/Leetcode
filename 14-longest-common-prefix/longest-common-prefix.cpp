4a9e2ae712e79b59e680ac2a2a11767a4601660b
Meet Sharma <itsmeetcodes@gmail.com>
2026-09-07 23:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (7.92%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        sort(strs.begin(),strs.end());
+        string start=strs[0];
+        string end=strs[strs.size()-1];
+        string ans="";
+        for(int i=0;i<min(start.length(),end.length());i++){
+            if(start[i]!=end[i]){
+                break;
+            }
+            ans=ans+start[i];
+        }
+        return ans;
+    }
+};
